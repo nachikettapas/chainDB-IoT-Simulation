222cd45479a43e433f0fd3c65b539d558811febf
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-06-26 22:24:52 +0200

    audrino is now able to ping local server

--- bigchaindb-plugin.js
+++ bigchaindb-plugin.js
@@ -3,7 +3,9 @@ exports.main = function (arguments){
 
     var name = arguments.name;
   
-    var LIGHTNINGROD_HOME = process.env.LIGHTNINGROD_HOME;
+	var LIGHTNINGROD_HOME = process.env.LIGHTNINGROD_HOME;
+	var http = require('http');
+	var requestify = require('requestify');
     api = require(LIGHTNINGROD_HOME + '/modules/plugins-manager/plugin-apis');
     logger = api.getLogger();
 
@@ -11,6 +13,13 @@ exports.main = function (arguments){
   
     setInterval(function(){ 
 	logger.info('Hello '+name+'!'); 
+	requestify.get('http://172.17.5.188:8000').then( function(response) {
+
+		var data = response.getBody();
+
+		logger.info(data);
+
+	});
       
     }, 3000); 
 };
