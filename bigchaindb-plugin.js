cf589a49ac58d38420c04bf9dbc5bd7548c1be48
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-06-26 22:33:26 +0200

    Vanity

--- bigchaindb-plugin.js
+++ bigchaindb-plugin.js
@@ -4,15 +4,22 @@ exports.main = function (arguments){
     var name = arguments.name;
   
 	var LIGHTNINGROD_HOME = process.env.LIGHTNINGROD_HOME;
+
 	var http = require('http');
 	var requestify = require('requestify');
+
+	var bigchaindbAddress = "http://172.17.5.188:8000";
+
     api = require(LIGHTNINGROD_HOME + '/modules/plugins-manager/plugin-apis');
     logger = api.getLogger();
 
-    logger.info("BigchainDB plugin starting...");
+    logger.info("BigchainDB plugin initialising...");
   
     setInterval(function(){ 
 	logger.info('Hello '+name+'!'); 
+
+
+
 	requestify.get('http://172.17.5.188:8000').then( function(response) {
 
 		var data = response.getBody();
