6673da468656e4bb434474a7345b1f11b6b31ca4
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-06-26 22:06:08 +0200

    Plugin version 0

---
+++ bigchaindb-plugin.js
@@ -0,0 +1,16 @@
+//pluginjsonschema = {"name": "BigchainDB"}
+exports.main = function (arguments){ 
+
+    var name = arguments.name;
+  
+    var LIGHTNINGROD_HOME = process.env.LIGHTNINGROD_HOME;
+    api = require(LIGHTNINGROD_HOME + '/modules/plugins-manager/plugin-apis');
+    logger = api.getLogger();
+
+    logger.info("BigchainDB plugin starting...");
+  
+    setInterval(function(){ 
+	logger.info('Hello '+name+'!'); 
+      
+    }, 3000); 
+};
