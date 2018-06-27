4dace37779686e3a1eedad8a6c64c80e6c80692a
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-06-27 11:36:30 +0200

    Server now handles post requests

--- custom-api/app/routes.py
+++ custom-api/app/routes.py
@@ -1,5 +1,13 @@
 from app import app
+from flask import request
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/sensor', methods= ['POST'])
+def sensor():
+    # print(request.data)
+    #curl -d "{'temp':'30'}" -H "Content-Type: application/json" -X POST  http://localhost:500
+    return request.data
