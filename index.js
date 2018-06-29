d3262832590a83eb4d08be4d6ac5a5fcacd9f9c6
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-06-29 11:55:31 +0200

    Testing set up for insertion in bigchaindb

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const BigchainDB = require('bigchaindb-driver');
 
-const API_PATH = 'http://127.0.0.1:6984/api/v1/';
+const API_PATH = 'http://localhost:9984/api/v1/';
 
 // Create a new keypair.
 const alice = new BigchainDB.Ed25519Keypair()
 
 // Construct a transaction payload
 
-
+const assetdata = {
+    'reading': {
+            'timestamp': new Date(),
+            'sensor'   : 'asda-dsvsvsd-213sdf-sdfsdf',
+            'type'     : 'temperature',
+            'reading'  : '30',
+            'Altitude' : '<reading>',
+            'Lattitude': '<reading>',
+            'Longitude': '<reading>'
+    }
+}
+const metadata= {'planet': 'earth'}
 // Send the transaction off to BigchainDB
 let conn = new BigchainDB.Connection(API_PATH, {
     app_id: 'c5f253ba',
     app_key: 'a9b4f9021b694736fdb463c9ba0d1d13'
 })
+
+const txCreateAliceSimple = BigchainDB.Transaction.makeCreateTransaction(
+    assetdata,
+    metadata,
+    // A transaction needs an output
+    [ BigchainDB.Transaction.makeOutput(
+            BigchainDB.Transaction.makeEd25519Condition(alice.publicKey))
+    ],
+    alice.publicKey
+)
+txCreateAliceSimpleSigned = BigchainDB.Transaction.signTransaction(txCreateAliceSimple, alice.privateKey)
+conn.postTransactionCommit(txCreateAliceSimpleSigned)
+
+
 // conn.postTransactionCommit(txSigned)
 //     .then(res => {
 //         console.log("n")        
@@ -23,9 +48,13 @@ let conn = new BigchainDB.Connection(API_PATH, {
 //         
 //     })
 // Check console for the transaction's status
-conn.searchAssets('2328976')
-            .then(assets => console.log('Found :', assets))
-var data = ["2328976","b","c","d","e"];
+
+
+// conn.searchAssets('2328976')
+//             .then(assets => console.log('Found :', assets))
+// var data = ["2328976","b","c","d","e"];
+
+
 
 // var processItems = function(x){
 //   if( x < data.length ) {
