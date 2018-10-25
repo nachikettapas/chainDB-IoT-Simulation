da4ae5ad4ec4b88c6a9b77b179f08c26ed0dbe1a
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-10-25 06:04:59 +0530

    Script v1

--- Simulation-Scripts/js-iot-node-simulation.js
+++ Simulation-Scripts/js-iot-node-simulation.js
@@ -1,5 +1,10 @@
-var requestify = require('requestify');
 
+var requestify = require('requestify');
+var parseArgs = require('minimist');
+var syncRequest = require('sync-request');
+
+var argv = parseArgs(process.argv.slice(2));
+console.log(argv);
 function generateRandomData(resource_id){
     data = []
     data.push({
@@ -52,14 +57,40 @@ function generateRandomData(resource_id){
         type: 'Pressure',
         resource_id: resource_id
     });
-    console.log('Data generated\n', data);
+    // console.log('Data generated\n', data);
     return data;
 }
 
+function sendSyncData(data) {
+    try{
+    var result = syncRequest('POST', 'http://212.189.207.149/bdb/sensors/93c39ba9-74cf-4461-b60a-9a206c7fc416', {json:data, timeout:3000});
+    console.log(result);
+    }
+    catch(err){
+        console.log('err');
+    }
+}
 
-// requestify.post(
-//     'http://212.189.207.119/bdb/sensors/93c39ba9-74cf-4461-b60a-9a206c7fc416',generateRandomData('93c39ba9-74cf-4461-b60a-9a206c7fc416'))
-//     .then(function(response) {
-//         console.log(response.getBody());
-//     });
-generateRandomData('93c39ba9-74cf-4461-b60a-9a206c7fc416');
+async function sendAsyncData(data){
+    let result = await requestify.post(
+        'http://212.189.207.149/bdb/sensors/93c39ba9-74cf-4461-b60a-9a206c7fc416',data);
+    console.log(result);
+}
+
+
+
+setInterval(function(){
+        for(i=0;i<argv.devices;i++){
+            data = generateRandomData('93c39ba9-74cf-4461-b60a-9a206c7fc415');
+            // console.log('===========\n==============\n');
+            if(argv.simultaneous=='true'){ 
+                console.log('mode:async',i);
+                sendAsyncData(data);
+            }
+            else {
+                console.log('mode:sync',i);
+                sendSyncData(data);
+            }
+        }
+   
+}, argv.interval*1000);
